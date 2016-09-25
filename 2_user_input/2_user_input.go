e7450df97653bc21c56556ffbefedef429b3c866
katecpp <kasia.macias@gmail.com>
2016-09-25 17:30:46 +0200

    Validate input when integer is expected

--- 2_user_input/2_user_input.go
+++ 2_user_input/2_user_input.go
@@ -8,18 +8,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+func readInt() int {
+	var i int
+	for _, err := fmt.Scanln(&i); err != nil; _, err = fmt.Scanln(&i) {
+		// If int read fails, read as string and forget
+		var discard string
+		fmt.Scanln(&discard)
+		fmt.Println("This is not a valid number! Try again...")
+	}
+	return i
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
 	fmt.Print("Whats your name? ")
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSpace(name)
 	fmt.Print("How old are you? ")
-	age, _ := reader.ReadString('\n')
-	age = strings.TrimSpace(age)
-
-	fmt.Println("Hello, " + name + ". You are " + age + " years old.")
+	age := readInt()
+	fmt.Println("Hello, " + name + ". You are " + strconv.Itoa(age) + " years old.")
 }
